2a987b7bc7337fa625c40ff1b3b17160d3ddfdbb
Andrés Pérez <andresepc5@gmail.com>
2026-04-21 15:09:17 -0500

    queries.sql

--- queries.sql
+++ queries.sql
@@ -26,14 +26,15 @@ FROM employees AS e
 INNER JOIN sales AS s ON e.employee_id = s.sales_person_id
 INNER JOIN products AS p ON s.product_id = p.product_id
 GROUP BY seller
-HAVING AVG(s.quantity * p.price) < (
-    SELECT
-        AVG(
-            s2.quantity * p2.price
-        )
-    FROM sales AS s2
-    INNER JOIN products AS p2 ON s2.product_id = p2.product_id
-)
+HAVING
+    AVG(s.quantity * p.price) < (
+        SELECT
+            AVG(
+                s2.quantity * p2.price
+            )
+        FROM sales AS s2
+        INNER JOIN products AS p2 ON s2.product_id = p2.product_id
+    )
 ORDER BY average_income ASC;
 
 -- Ingreso por vendedor por día
